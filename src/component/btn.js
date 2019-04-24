713fd8b6dbf159f61f215bde5dbf1dc36be5d1cd
Sabrina <hsif0111@gmail.com>
2019-04-24 10:31:57 +0800

    newCreat

---
+++ src/component/btn.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+export default class Btn extends Component {
+  
+
+  toggle() {
+   
+  }
+
+  render() {
+    // console.log(this.props); //button object
+    console.log(this.props.openAtStart);
+    return (
+      <div onClick={() => this.toggle()} className={this.props.button.class}>
+        {
+          this.props.button.closeText
+          /* {openAtStart === true
+          ? this.props.button.closeText
+          : this.props.button.closeText} */
+        }
+      </div>
+    );
+    
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Banner from "./Banner";
+
+ReactDOM.render( < Banner / > , document.getElementById("root"));
